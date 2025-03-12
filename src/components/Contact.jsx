fe6a8099f32c3a9db07c4043cb8fe83203fd4880
Dipali Makadia <dipali.makadia@gmail.com>
2025-03-12 10:33:14 -0700

    Changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,8 +19,8 @@ const Contact = () => {
         <div className=" flex flex-col justify-center items-center">
           <p className="text-3xl py-5">Dipali Makadia</p>
           {/* <p className="text-2xl py-5">562-825-8686</p> */}
-          <p className="text-2xl py-5">dipali.makadia@gmail.com</p>
-          <p className="text-2xl py-5">Los Angeles, United States</p>
+          {/* <p className="text-2xl py-5">dipali.makadia@gmail.com</p> */}
+          <p className="text-2xl py-5">United States</p>
         </div>
         <div className="flex justify-center items-center">
           <a
